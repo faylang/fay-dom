55e045ffdfa595154cd496fceb3f860d725cfc41
Anupam Jain <ajnsit@gmail.com>
2016-05-10 13:11:58 +0530

    Bugfix!

--- src/DOM.hs
+++ src/DOM.hs
@@ -41,7 +41,7 @@ getElementsByName :: Text -> Fay [Element]
 getElementsByName = ffi "document['getElementsByName'](%1)"
 
 addEvent :: Element -> Text -> Fay f -> Fay ()
-addEvent = ffi "%1['addEventListener'](%1,%2)"
+addEvent = ffi "%1['addEventListener'](%2,%3)"
 
 removeEvent :: Element -> Text -> (Event -> Fay f) -> Fay ()
 removeEvent = ffi "%1['removeEventListener'](%2, %3)"
